14ff9e060d03c390222346c57af7af90b33b5131
Shians <registertonysu@gmail.com>
2018-02-07 09:03:14 +1100

    Restructured data

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
-var map = L.map('map').setView([-37.81, 144.95], 14);
-mapLink = 
+var map = L.map('map', {zoomControl:false}).setView([-37.81, 144.95], 14);
+mapLink =
 '<a href="http://www.arcgis.com">Stamen Design</a>';
 L.tileLayer(
     'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {
     attribution: '&copy; ' + mapLink,
-    maxZoom: 18,
+    minZoom: 10,
+    maxZoom: 16
 }).addTo(map);
 
 /* Initialize the SVG layer */
-map._initPathRoot()    
+map._initPathRoot()
 
 /* We simply pick up the SVG from the map object */
 var svg = d3.select("#map").select("svg"),
 g = svg.append("g");
 
-console.log(collection)
+/* Initialize tooltip */
+tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d.Sensor + "<br />" + "(" + d.Longitude + ", " + d.Latitude + ")"; });
+
+/* Invoke the tip in the context of your visualization */
+svg.call(tip)
 
 /* Add a LatLng object to each item in the dataset */
-collection.objects.forEach(function(d) {
-    d.LatLng = new L.LatLng(d.circle.coordinates[0],
-        d.circle.coordinates[1])
-    })
-    
-    var feature = g.selectAll("circle")
-    .data(collection.objects)
+sensors.forEach(function(d) {
+    d.LatLng = new L.LatLng(
+        d.Latitude,
+        d.Longitude
+    )
+})
+
+// draw points
+var feature = g.selectAll("circle")
+    .data(sensors)
     .enter().append("circle")
-    .style("stroke", "black")  
-    .style("opacity", 1) 
+    .style("stroke", "black")
+    .style("opacity", 1)
     .style("fill", "green")
     .attr("r", 5)
-    .on("mouseover", function(d) {console.log("over")});
-    
-    map.on("viewreset", update);
-    update();
-    
-    function update() {
-        feature.attr("transform", 
-        function(d) { 
-            return "translate("+ 
-            map.latLngToLayerPoint(d.LatLng).x +","+ 
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide);
+
+map.on("viewreset", update);
+update();
+
+function update() {
+    feature.attr("transform",
+        function(d) {
+            return "translate("+
+            map.latLngToLayerPoint(d.LatLng).x +","+
             map.latLngToLayerPoint(d.LatLng).y +")";
         }
     )
