7fa409460c5e7c571b0f70da823e49750693d13b
Shians <registertonysu@gmail.com>
2018-02-08 13:34:45 +1100

    Added slider

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var sensorData;
 d3.csv("./counts_all_2017.csv", function (error, data) {
     dailyData = data;
     dailyData = _.filter(dailyData, d => { return d.Count != "NA" });
+    
     d3.csv("./sensors.csv", function (error, data) {
         sensorData = data;
         drawMap(sensorData);
--- utilities.js
+++ utilities.js
@@ -8,3 +8,16 @@ function precisionRound(number, precision) {
 const numberWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+// taken from https://stackoverflow.com/questions/4048688/how-can-i-convert-day-of-year-to-date-in-javascript
+// get a date object from a year and a day-in-year
+function dateFromDay(year, day){
+    var date = new Date(year, 0); // initialize a date in `year-01-01`
+    return new Date(date.setDate(day)); // add the number of days
+}
+
+// taken from Shian's brain
+function dateFromYMD(date) {
+    var format = d3.time.format("%Y-%m-%d");
+    return format.parse(date);
+}
